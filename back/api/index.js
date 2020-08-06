6ae28148f787dfe593ffeace5a6e4406c10bf222
SeoSang <ddrrpg@naver.com>
2020-08-06 14:44:32 +0900

    api로 리그 가져오는 upcoming 일단 구현

---
+++ back/api/index.js
@@ -0,0 +1,7 @@
+const API_URL = 'https://api-football-v1.p.rapidapi.com/v2';
+const API_KEY = '9759ae1d35msh996d1ea0071d8b7p14702ajsnf6f744b030f4';
+
+module.exports = {
+  API_URL: API_URL,
+  API_KEY: API_KEY,
+};
