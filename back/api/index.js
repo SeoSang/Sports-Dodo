41d2f2b673ebcb3823178da3a05a900aaf9602ab
SeoSang <ddrrpg@naver.com>
2020-08-06 15:27:39 +0900

    키 삭제

--- back/api/index.js
+++ back/api/index.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
+
 const API_URL = 'https://api-football-v1.p.rapidapi.com/v2';
-const API_KEY = '9759ae1d35msh996d1ea0071d8b7p14702ajsnf6f744b030f4';
+const API_KEY = process.env.API_KEY;
 
 module.exports = {
   API_URL: API_URL,
