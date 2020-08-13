6937219f2dd454f5405f03ab7a58fb045a77d8f6
SeoSang <ddrrpg@naver.com>
2020-08-13 23:53:55 +0900

    ranking saga 도입

--- front/reducers/index.js
+++ front/reducers/index.js
@@ -1,10 +1,14 @@
 // 하위 리듀서들 하나로 묶어주는 index
 
-import { combineReducers } from "redux"
-import user from "./user.js"
+import { combineReducers } from 'redux';
+import user from './user.js';
+import batting from './batting.js';
+import ranking from './ranking.js';
 
 const rootReducer = combineReducers({
   user,
-})
+  ranking,
+  batting,
+});
 
-export default rootReducer
+export default rootReducer;
--- front/sagas/index.js
+++ front/sagas/index.js
@@ -1,11 +1,12 @@
 import { all, call } from 'redux-saga/effects';
 import userSaga from './user';
 import battingSaga from './batting';
+import rankingSaga from './ranking';
 import axios from 'axios';
 import { BACKEND_URL } from '../src/dummy.js';
 
 axios.defaults.baseURL = `${BACKEND_URL}/api`;
 
 export default function* rootSaga() {
-  yield all([call(userSaga), call(battingSaga)]);
+  yield all([call(userSaga), call(battingSaga), call(rankingSaga)]);
 }
