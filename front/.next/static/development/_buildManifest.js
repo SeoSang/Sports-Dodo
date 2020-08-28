48d752d59435d581038aab93d0a2fa153004f449
SeoSang <ddrrpg@naver.com>
2020-08-28 10:49:42 +0900

    morgan 위치 잘못돼있던거 수정

--- front/.next/static/development/_buildManifest.js
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
-=======
-self.__BUILD_MANIFEST = {"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/matchs":["static\u002Fchunks\u002Fpages\u002Fmatchs.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
->>>>>>> 46b07af96106e3b880a970e51507d460dec477a1
