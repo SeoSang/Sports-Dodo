dca52206189a3c3d19ee990770d5956d9d1bc9b1
SeoSang <ddrrpg@naver.com>
2020-09-09 14:32:55 +0900

    컴퍼넌트들 jsx 화

--- front/components/MainMenu.jsx
+++ front/components/MainMenu.jsx
@@ -23,7 +23,14 @@ const MainMenu = ({ visible }) => {
   };
 
   return (
-    <div style={{ zIndex: 100, position: 'absolute', width: 256, top: '70px' }}>
+    <div
+      style={{
+        zIndex: 100,
+        position: 'absolute',
+        width: 256,
+        top: '70px',
+      }}
+    >
       {visible ? (
         <Menu
           defaultSelectedKeys={['1']}
@@ -37,42 +44,46 @@ const MainMenu = ({ visible }) => {
           {/* //홈 추가  홈링크 왜 안가지는지 모르겠음*/}
           <Menu.Item key="0" icon={<HomeOutlined />}>
             <Link href="/">
-              <a>홈</a>
+              <a> 홈 </a>
             </Link>
           </Menu.Item>
           <SubMenu key="sub1" icon={<DesktopOutlined />} title="매칭 보기">
             <Menu.Item key="1">
               <Link href="/matchs">
-                <a>축구</a>
+                <a> 축구 </a>
               </Link>
             </Menu.Item>
-            <Menu.Item key="2">야구</Menu.Item>
-            <Menu.Item key="3">농구</Menu.Item>
+            <Menu.Item key="2"> 야구 </Menu.Item>
+            <Menu.Item key="3"> 농구 </Menu.Item>
           </SubMenu>
           <Menu.Item key="4" icon={<ContainerOutlined />}>
-            <Link href="/ranking">
-              <a>베팅 내역</a>
+            <Link href="/rankings">
+              <a> 베팅 내역 </a>
             </Link>
           </Menu.Item>
           <Menu.Item key="5" icon={<PieChartOutlined />}>
-            <Link href="/ranking">
-              <a>전체 랭킹</a>
+            <Link href="/rankings">
+              <a> 전체 랭킹 </a>
             </Link>
           </Menu.Item>
           <Menu.Item key="6" icon={<AppstoreOutlined />}>
-            기타 메뉴 (아직 쓸지 안쓸지 모름)
+            기타 메뉴(아직 쓸지 안쓸지 모름)
           </Menu.Item>
           <SubMenu key="sub2" icon={<MailOutlined />} title="커뮤니티">
-            <Menu.Item key="7">커뮤니티 1</Menu.Item>
-            <Menu.Item key="8">커뮤니티 2</Menu.Item>
+            <Menu.Item key="7"> 커뮤니티 1 </Menu.Item>
+            <Menu.Item key="8"> 커뮤니티 2 </Menu.Item>
             <SubMenu key="sub3" title="Submenu">
-              <Menu.Item key="9">기타 1</Menu.Item>
-              <Menu.Item key="10">기타 2</Menu.Item>
+              <Menu.Item key="9"> 기타 1 </Menu.Item>
+              <Menu.Item key="10"> 기타 2 </Menu.Item>
             </SubMenu>
           </SubMenu>
         </Menu>
       ) : (
-        <div style={{ display: 'none' }}></div>
+        <div
+          style={{
+            display: 'none',
+          }}
+        ></div>
       )}
     </div>
   );
--- front/pages/rankings.jsx
+++ front/pages/rankings.jsx
@@ -3,16 +3,14 @@ import { Row, Col } from 'antd';
 import styled from 'styled-components';
 import MatchLine from '../components/MatchLine';
 import { useSelector, useDispatch } from 'react-redux';
-import { LOAD_MATCHS_REQUEST } from '../sagas/match';
-import { wrapper } from '../store';
-import Link from 'next/link';
+import { LOAD_Rankings_REQUEST } from '../sagas/match';
 
-const MatchsTitleRow = styled(Row)`
+const RankingsTitleRow = styled(Row)`
   background-color: #b6dbf2;
   padding: 10px;
 `;
 
-const MatchsContentRow = styled(Row)`
+const RankingsContentRow = styled(Row)`
   background-color: white;
   min-height: 80vh;
   padding: 10px;
@@ -26,20 +24,25 @@ const dummy_matchLine = {
   deadline: new Date().toString(),
 };
 
-const matchs = () => {
+const rankings = () => {
   const state = useSelector((state) => state.match);
-  const { matchs } = useSelector((state) => state.match);
+  const { Rankings } = useSelector((state) => state.match);
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch({ type: LOAD_MATCHS_REQUEST });
+    dispatch({
+      type: LOAD_Rankings_REQUEST,
+    });
   }, []);
   return (
     <Row
-      style={{ textAlign: 'center', padding: '20px' }}
+      style={{
+        textAlign: 'center',
+        padding: '20px',
+      }}
       justify="space-around"
     >
       <Row>
-        <h1>매치 보기</h1>
+        <h1> 매치 보기 </h1>
       </Row>
       <Row
         style={{
@@ -47,38 +50,38 @@ const matchs = () => {
             '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
         }}
       >
-        <MatchsTitleRow>
+        <RankingsTitleRow>
           <Col span={4}>
-            <h2>홈</h2>
+            <h2> 홈 </h2>
           </Col>
           <Col span={4}>
-            <h2>어웨이</h2>
+            <h2> 어웨이 </h2>
           </Col>
           <Col span={5}>
-            <h2>경기시간</h2>
+            <h2> 경기시간 </h2>
           </Col>
           <Col span={4}>
-            <h2>현황</h2>
+            <h2> 현황 </h2>
           </Col>
           <Col span={5}>
-            <h2>마감</h2>
+            <h2> 마감 </h2>
           </Col>
           <Col span={2}>
-            <h2>베팅</h2>
+            <h2> 베팅 </h2>
           </Col>
-        </MatchsTitleRow>
-        <MatchsContentRow>
-          {matchs?.map((match) => (
-            <MatchLine {...match}></MatchLine>
+        </RankingsTitleRow>
+        <RankingsContentRow>
+          {Rankings?.map((match) => (
+            <MatchLine {...match}> </MatchLine>
           ))}
-        </MatchsContentRow>
+        </RankingsContentRow>
       </Row>
     </Row>
   );
 };
 
 // export const getStaticProps = wrapper.getStaticProps(async ({ store }) => {
-//   store.dispatch({ type: LOAD_MATCHS_REQUEST, index: 1 });
+//   store.dispatch({ type: LOAD_Rankings_REQUEST, index: 1 });
 // });
 
-export default matchs;
+export default rankings;
