c3fe27469b7136f141f83ae46e427a2543058698
SeoSang <ddrrpg@naver.com>
2020-09-14 19:18:45 +0900

    랭킹페이지 데이터맞게 수정

--- front/components/RankingLine.jsx
+++ front/components/RankingLine.jsx
@@ -2,14 +2,26 @@ import React from 'react';
 import { Row, Col, Button } from 'antd';
 import Link from 'next/link';
 
-const RankingLine = ({ _id, rank, nickname, success, fail }) => {
+const calPercent = (win, lose) => {
+  if (win === 0 && lose === 0) return 0;
+  if (lose == 0) return 100;
+  else return Math.round((win / (win + lose)) * 1000) / 10;
+};
+
+const RankingLine = ({ _id, rank, point, nickname, battings }) => {
   return _id ? (
     <Row>
       <Col span={2}>{rank}</Col>
-      <Col span={10}>{nickname}</Col>
-      <Col span={2}>{3}회</Col>
-      <Col span={2}>{0}회</Col>
-      <Col span={4}>성공률</Col>
+      <Col span={8}>{nickname}</Col>
+      <Col span={3}>{Math.round(point)}p</Col>
+      <Col span={2}>{battings[0].collectCount}회</Col>
+      <Col span={2}>{battings[0].wrongCount}회</Col>
+      <Col span={3}>
+        {battings
+          ? calPercent(battings[0].collectCount, battings[0].wrongCount)
+          : '데이터없음'}
+        %
+      </Col>
       <Col span={4}>
         <Link href={{ pathname: 'userprofile', query: { userId: _id } }}>
           <a>
@@ -23,8 +35,11 @@ const RankingLine = ({ _id, rank, nickname, success, fail }) => {
       <Col span={2}>
         <h2>순위</h2>
       </Col>
-      <Col span={10}>
+      <Col span={8}>
         <h2>닉네임</h2>
+      </Col>
+      <Col span={3}>
+        <h2>포인트</h2>
       </Col>
       <Col span={2}>
         <h2>성공</h2>
@@ -32,7 +47,7 @@ const RankingLine = ({ _id, rank, nickname, success, fail }) => {
       <Col span={2}>
         <h2>실패</h2>
       </Col>
-      <Col span={4}>
+      <Col span={3}>
         <h2>성공률</h2>
       </Col>
       <Col span={4}>
