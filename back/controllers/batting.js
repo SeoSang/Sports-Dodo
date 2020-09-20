e7ea499f44a453b9953d5812735051c590a25695
moonQ <mooongq@gmail.com>
2020-09-20 15:35:56 +0900

    all work but need refactoring

--- back/controllers/batting.js
+++ back/controllers/batting.js
@@ -7,7 +7,6 @@ const Batting = require('../models/Batting');
 const Match = require('../models/Match');
 
 exports.createBatting = asyncHandler(async (req, res, next) => {
-<<<<<<< HEAD
     const user = req.user._id;
     const match = req.params.id;
 
@@ -43,72 +42,38 @@ exports.createBatting = asyncHandler(async (req, res, next) => {
         data: batting
     })
 })
-=======
-  const user = req.user._id;
-  const match = req.params.id;
-
-  const chooseHomeAwayDraw = req.body.chooseHomeAwayDraw;
-  const battingPoint = req.body.battingPoint;
-  const description = req.body.description;
-
-  const batting = await Batting.create({
-    user: user,
-    match: match,
-    chooseHomeAwayDraw: chooseHomeAwayDraw,
-    battingPoint: battingPoint,
-    description: description,
-  });
-
-  await User.findByIdAndUpdate(
-    req.user._id,
-    { $inc: { point: -`${battingPoint}` } },
-    { new: true }
-  );
-  await Match.findByIdAndUpdate(
-    req.params.id,
-    { $inc: { howManyPeopleBatted: +1 } },
-    { new: true }
-  );
-
-  return res.status(201).json({
-    success: true,
-    data: batting,
-  });
-});
->>>>>>> upstream/master
 
 // model에 index 넣어주면 더 빨라질듯함.
 exports.getBattings = asyncHandler(async (req, res, next) => {
-  const battings = await Batting.find({ match: req.params.id });
+    const battings = await Batting.find({ match: req.params.id });
 
-  //homeTotal, awayTotal, drawTotal Point 계산하기
-  let homeTotalPoint = 0;
-  let drawTotalPoint = 0;
-  let awayTotalPoint = 0;
+    //homeTotal, awayTotal, drawTotal Point 계산하기
+    let homeTotalPoint = 0;
+    let drawTotalPoint = 0;
+    let awayTotalPoint = 0;
 
-  for (let i = 0; i < battings.length; i++) {
-    if (battings[i].chooseHomeAwayDraw === 'Home') {
-      homeTotalPoint += battings[i].battingPoint;
-    } else if (battings[i].chooseHomeAwayDraw === 'Away') {
-      awayTotalPoint += battings[i].battingPoint;
-    } else {
-      drawTotalPoint += battings[i].battingPoint;
+    for (let i = 0; i < battings.length; i++) {
+        if (battings[i].chooseHomeAwayDraw === 'Home') {
+            homeTotalPoint += battings[i].battingPoint;
+        } else if (battings[i].chooseHomeAwayDraw === 'Away') {
+            awayTotalPoint += battings[i].battingPoint;
+        } else {
+            drawTotalPoint += battings[i].battingPoint;
+        }
     }
-  }
 
-  return res.status(200).json({
-    success: true,
-    // data: battings,
-    battingPoints: {
-      homeTotalPoint: homeTotalPoint,
-      awayTotalPoint: awayTotalPoint,
-      drawTotalPoint: drawTotalPoint,
-    },
-    howManyPeopleBatted: battings.length,
-  });
+    return res.status(200).json({
+        success: true,
+        // data: battings,
+        battingPoints: {
+            homeTotalPoint: homeTotalPoint,
+            awayTotalPoint: awayTotalPoint,
+            drawTotalPoint: drawTotalPoint,
+        },
+        howManyPeopleBatted: battings.length,
+    });
 });
 
-<<<<<<< HEAD
 exports.getRecords = asyncHandler(async (req, res, next) => {
     let battings = await Batting.find({ user: req.params.id })
         .sort({ createdAt: -1 })
@@ -119,89 +84,72 @@ exports.getRecords = asyncHandler(async (req, res, next) => {
         data: battings
     });
 })
-=======
-// getmybattings
-exports.getMyBattings = asyncHandler(async (req, res, next) => {
-  const collectBattings = await Batting.find(
-    { user: req.user._id },
-    { battingResult: 'Collect' }
-  );
-  const wrongBattings = await Batting.find(
-    { user: req.user._id },
-    { battingResult: 'Wrong' }
-  );
-  const notFinishedBattings = await Batting.find(
-    { user: req.user._id },
-    { battingResult: 'Not Finished' }
-  );
-});
->>>>>>> upstream/master
 
 exports.getBatting = asyncHandler(async (req, res, next) => {
-  const batting = await Batting.findById(req.params.id);
+    const batting = await Batting.findById(req.params.id);
 
-  if (!batting) {
-    return next(
-      new ErrorResponse(`No batting with the id of ${req.params.id}`, 404)
-    );
-  }
+    if (!batting) {
+        return next(
+            new ErrorResponse(`No batting with the id of ${req.params.id}`, 404)
+        );
+    }
 
-  //
+    //
 
-  return res.status(200).json({
-    success: true,
-    batting: batting,
-  });
+    return res.status(200).json({
+        success: true,
+        batting: batting,
+    });
 });
 
 exports.editBatting = asyncHandler(async (req, res, next) => {
-  let batting = await Batting.findById(req.params.id);
+    let batting = await Batting.findById(req.params.id);
 
-  if (!batting) {
-    return next(
-      new ErrorResponse(`No Batting with the id of ${req.params.id}`, 404)
-    );
-  }
+    if (!batting) {
+        return next(
+            new ErrorResponse(`No Batting with the id of ${req.params.id}`, 404)
+        );
+    }
 
-  batting = await Batting.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-    runValidators: true,
-  });
+    batting = await Batting.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+    });
 
-  batting.save();
+    batting.save();
 
-  res.status(200).json({
-    success: true,
-    data: batting,
-  });
+    res.status(200).json({
+        success: true,
+        data: batting,
+    });
 });
 
 exports.deleteBatting = asyncHandler(async (req, res, next) => {
-  let batting = await Batting.findById(req.params.id);
+    let batting = await Batting.findById(req.params.id);
 
-  if (!batting) {
-    return next(
-      new ErrorResponse(`No Batting with the id of ${req.params.id}`, 404)
+    if (!batting) {
+        return next(
+            new ErrorResponse(`No Batting with the id of ${req.params.id}`, 404)
+        );
+    }
+
+    // +battingPoint back to UserModel
+    await User.findByIdAndUpdate(
+        batting.user,
+        { $inc: { point: +`${batting.battingPoint}` } },
+        { new: true }
     );
-  }
+    // -howManyPeopleBatted back to MatchModel
+    await Match.findByIdAndUpdate(
+        batting.match,
+        { $inc: { howManyPeopleBatted: -1 } },
+        { new: true }
+    );
 
-  // +battingPoint back to UserModel
-  await User.findByIdAndUpdate(
-    batting.user,
-    { $inc: { point: +`${batting.battingPoint}` } },
-    { new: true }
-  );
-  // -howManyPeopleBatted back to MatchModel
-  await Match.findByIdAndUpdate(
-    batting.match,
-    { $inc: { howManyPeopleBatted: -1 } },
-    { new: true }
-  );
+    await Batting.deleteOne({ _id: req.params.id });
 
-  await Batting.deleteOne({ _id: req.params.id });
-
-  res.status(200).json({
-    success: true,
-    data: {},
-  });
+    res.status(200).json({
+        success: true,
+        data: {},
+    });
 });
