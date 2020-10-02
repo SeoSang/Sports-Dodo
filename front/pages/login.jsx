9e46326766ef631ce6d788c27ce9822a40a44266
SeoSang <ddrrpg@naver.com>
2020-10-02 16:02:36 +0900

    로그인 페이지 에러 해결, 메뉴 라우터 오류 해결

--- front/pages/login.jsx
+++ front/pages/login.jsx
@@ -1,4 +1,4 @@
-import { FullDiv } from '../styles/styled-components';
+import { FlexDiv, FullDiv } from '../styles/styled-components';
 import { Button, Input, Checkbox, Form, message } from 'antd';
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
 import Link from 'next/link';
@@ -22,10 +22,7 @@ const tailLayout = {
 };
 
 const loginFormStyle = {
-  top: '50%',
-  position: 'relative',
-  transform: 'translate(0%,-80%)',
-
+  padding: '5vh 0',
 };
 
 const cookie = new Cookies();
@@ -66,71 +63,73 @@ const login = () => {
   };
 
   return (
-    <FullDiv style={{ marginTop: '5vh',backgroundColor:'white' }}>
-      {/* <Button onClick={onClickTest}>핑 테스트</Button> */}
+    <FlexDiv width="100%" height="100%">
+      <FullDiv style={{ marginTop: '5vh' }}>
+        {/* <Button onClick={onClickTest}>핑 테스트</Button> */}
 
-      <Form
-        {...layout}
-        style={loginFormStyle}
-        name="basic"
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-      >
-        <h1>로그인</h1>
-        <Form.Item
-          label="이메일"
-          name="email"
-          rules={[{ required: true, message: '이메일을 입력해주세요!' }]}
+        <Form
+          {...layout}
+          style={loginFormStyle}
+          name="basic"
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
         >
-          <Input
-            prefix={<UserOutlined className="site-form-item-icon" />}
-            placeholder="E-mail"
-          />
-        </Form.Item>
+          <h1>로그인</h1>
+          <Form.Item
+            label="이메일"
+            name="email"
+            rules={[{ required: true, message: '이메일을 입력해주세요!' }]}
+          >
+            <Input
+              prefix={<UserOutlined className="site-form-item-icon" />}
+              placeholder="E-mail"
+            />
+          </Form.Item>
 
-        <Form.Item
-          label="비밀번호"
-          name="password"
-          rules={[
-            {
-              required: true,
-              message: 'Please input your password!',
-            },
-          ]}
-        >
-          <Input
-            prefix={<LockOutlined className="site-form-item-icon" />}
-            type="password"
-            placeholder="Password"
-          />
-        </Form.Item>
+          <Form.Item
+            label="비밀번호"
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your password!',
+              },
+            ]}
+          >
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Item>
 
-        <Form.Item {...tailLayout} name="remember" valuePropName="checked">
-          <Checkbox>아이디 기억하기</Checkbox>
-        </Form.Item>
+          <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+            <Checkbox>아이디 기억하기</Checkbox>
+          </Form.Item>
 
-        <Form.Item {...tailLayout}>
-          <div>
-            <Button
-              type="primary"
-              htmlType="submit"
-              style={{ margin: '0 5px' }}
-            >
-              로그인
-            </Button>
-            <Button type="primary" style={{ margin: '0 5px' }}>
-              <Link href="/register">
-                <a>회원가입</a>
-              </Link>
-            </Button>
-            <Button type="primary" style={{ margin: '0 5px' }}>
-              비밀번호 찾기
-            </Button>
-          </div>
-        </Form.Item>
-      </Form>
-    </FullDiv>
+          <Form.Item {...tailLayout}>
+            <div>
+              <Button
+                type="primary"
+                htmlType="submit"
+                style={{ margin: '0 5px' }}
+              >
+                로그인
+              </Button>
+              <Button type="primary" style={{ margin: '0 5px' }}>
+                <Link href="/register">
+                  <a>회원가입</a>
+                </Link>
+              </Button>
+              <Button type="primary" style={{ margin: '0 5px' }}>
+                비밀번호 찾기
+              </Button>
+            </div>
+          </Form.Item>
+        </Form>
+      </FullDiv>
+    </FlexDiv>
   );
 };
 
